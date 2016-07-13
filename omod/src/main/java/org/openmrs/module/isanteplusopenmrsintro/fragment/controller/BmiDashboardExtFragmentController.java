d835fc86e77ec609166b6f4d808ccadf0bdb9984
jamesfeshner <jamesfeshner@yahoo.fr>
2016-07-13 16:54:27 -0400

    Update BmiDashboardExtFragmentController.java

--- omod/src/main/java/org/openmrs/module/isanteplusopenmrsintro/fragment/controller/BmiDashboardExtFragmentController.java
+++ omod/src/main/java/org/openmrs/module/isanteplusopenmrsintro/fragment/controller/BmiDashboardExtFragmentController.java
@@ -1,5 +1,7 @@
 package org.openmrs.module.isanteplusopenmrsintro.fragment.controller;
 
+import java.text.DecimalFormat;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.json.JSONArray;
@@ -13,40 +15,23 @@ public class BmiDashboardExtFragmentController {
 	protected final Log log = LogFactory.getLog(getClass());
 
 	public void controller(FragmentModel model, @FragmentParam("patientId") Patient patient) {
-		//JSONArray bmi = Context.getService(IsantePlusOmrsIntroService.class).getPatientWeights(patient);
+		DecimalFormat df = new DecimalFormat("###.#" ); 
 		
-		/*String[] bmiLabels = new String[bmi.length()];
-		Double[] bmiValues = new Double[bmi.length()];
-		Double[] weightsValues = new Double[bmi.length()];
-		Double[] heightsValues = new Double[bmi.length()];
-		for (int i = 0; i < bmi.length(); i++) {
-			    weightsValues[i] = bmi.getJSONObject(i).getDouble("weight");
-				heightsValues[i] = bmi.getJSONObject(i).getDouble("height");
-				bmiValues[i] = weightsValues[i] / ((heightsValues[i]/100) * (heightsValues[i]/100));
-				bmiLabels[i] = bmi.getJSONObject(i).getString("date");
-			
-		}*/
-		JSONArray heights = Context.getService(IsantePlusOmrsIntroService.class).getPatientHeights(patient);
+		Double heights = Context.getService(IsantePlusOmrsIntroService.class).getPatientHeights(patient);
 		JSONArray weights = Context.getService(IsantePlusOmrsIntroService.class).getPatientWeights(patient);
 		Double[] weightsValues = new Double[weights.length()];
-		Double[] heightsValues = new Double[heights.length()];
-		//String[] weightsLabels = new String[weights.length()];
-		//String[] heightsLabels = new String[heights.length()];
-		String[] bmiLabels = null;
-		Double[] bmiValues = null;
+		String[] bmiLabels = new String[weights.length()];
+		Double[] bmiValues = new Double[weights.length()];
 		for (int i = 0; i < weights.length(); i++) {
-			if(heights.getJSONObject(i).getString("date")==weights.getJSONObject(i).getString("date"))
-			{
-			    weightsValues[i] = weights.getJSONObject(i).getDouble("weight");
-				heightsValues[i] = heights.getJSONObject(i).getDouble("height");
-				bmiValues[i] = weightsValues[i] / ((heightsValues[i]/100) * (heightsValues[i]/100));
-				bmiLabels[i] = heights.getJSONObject(i).getString("date");
-			}
+			weightsValues[i] = weights.getJSONObject(i).getDouble("weight");
+			    bmiValues[i] = Double.parseDouble(df.format(weightsValues[i] / ((heights/100) * (heights/100))));
+				bmiLabels[i] = weights.getJSONObject(i).getString("date");
+			
 		
 		}
 		
 		model.addAttribute("bmiLabels", bmiLabels);
-		model.addAttribute("bmiValues", bmiValues);
+		model.addAttribute("bmiValues",  bmiValues);
 	}
 	
 	
